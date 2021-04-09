bd8f322caafaf0afc321acb7b24326da3dd9d4f5
janvr1 <jan.vrhovec@gmail.com>
2021-04-09 11:33:39 +0200

    BIG update

--- calculator.js
+++ calculator.js
@@ -11,11 +11,17 @@ const FUELS = {
         V: 7.700,
         H: 8.820,
         F: 0.8730,
+        CO2max: 11.8,
+        A1: 0.37,
+        B: 0.009,
     },
     heizol_EL: {
         V: 10.375,
         H: 11.86,
         F: 0.8748,
+        CO2max: 15.4,
+        A1: 0.5,
+        B: 0.007,
     }
 }
 
@@ -47,16 +53,38 @@ function calculate(e) {
 
     // Splošni podatki
     var o2 = document.getElementById("input_O2").value;
+    var t_air = document.getElementById("input_Tair").value;
+    var t_gas = document.getElementById("input_Tgas").value;
     var calc_o2 = document.getElementById("calc_o2ref").value;
     var inp_o2ref = document.getElementById("input_o2ref").value;
-    var fuel = document.getElementById("input_fuel").value;
+    var fuel = FUELS[document.getElementById("input_fuel").value];
 
     console.log("Splošni podatki:");
     console.log("O2: " + o2);
     console.log("Calc O2: " + calc_o2);
     console.log("Input O2ref: " + inp_o2ref);
     console.log("Fuel: " + fuel);
+    console.log("T_air:" + t_air);
+    console.log("T_gas:" + t_gas);
     console.log("----------------------");
+
+    // Splošni izračuni
+    if (o2 != "") {
+        document.getElementById("output_lambda").innerHTML = Math.round(lambda(o2) * 100) / 100;
+        document.getElementById("output_co2").innerHTML = Math.round(co2(fuel, o2) * 10) / 10;
+    } else {
+        document.getElementById("output_lambda").innerHTML = "--";
+        document.getElementById("output_co2").innerHTML = "--";
+
+    }
+    if (o2 != "" && t_gas != "" && t_air != "") {
+        let loss = Math.round(losses(t_gas, t_air, o2, fuel));
+        document.getElementById("output_loss").innerHTML = loss;
+        document.getElementById("output_eff").innerHTML = 100 - loss;
+    } else {
+        document.getElementById("output_loss").innerHTML = "--";
+        document.getElementById("output_eff").innerHTML = "--";
+    }
 
     // Izračun CO
     var unit_CO = document.getElementById("unit_CO").value;
@@ -71,7 +99,7 @@ function calculate(e) {
     } else if (unit_CO == "mg_m3_o2" && inp_o2ref != "") {
         co_ppm = co / (GASES.CO["mg_m3ppm"] * o2_ref(inp_o2ref, o2));
     } else if (unit_CO == "mg_kwh") {
-        co_ppm = co / (GASES.CO["mg_m3ppm"] * lambda(o2) * FUELS[fuel]["F"]);
+        co_ppm = co / (GASES.CO["mg_m3ppm"] * lambda(o2) * fuel["F"]);
     } else {
         co_ppm = 0;
     }
@@ -89,7 +117,7 @@ function calculate(e) {
         document.getElementById("output_CO_m3o2").innerHTML = "--"
     }
     if (o2 != "") {
-        document.getElementById("output_CO_kwh").innerHTML = Math.round(mg_kwh(co_mgm3, o2, FUELS[fuel].F) * 10) / 10;
+        document.getElementById("output_CO_kwh").innerHTML = Math.round(mg_kwh(co_mgm3, o2, fuel.F) * 10) / 10;
     } else {
         document.getElementById("output_CO_kwh").innerHTML = "--"
     }
@@ -108,7 +136,7 @@ function calculate(e) {
     } else if (unit_NO == "mg_m3_o2" && inp_o2ref != "") {
         no_ppm = no / (GASES.NO["mg_m3ppm"] * o2_ref(inp_o2ref, o2));
     } else if (unit_NO == "mg_kwh") {
-        no_ppm = no / (GASES.NO["mg_m3ppm"] * lambda(o2) * FUELS[fuel]["F"]);
+        no_ppm = no / (GASES.NO["mg_m3ppm"] * lambda(o2) * fuel["F"]);
     } else {
         no_ppm = 0;
     }
@@ -126,7 +154,7 @@ function calculate(e) {
     }
 
     if (o2 != "") {
-        document.getElementById("output_NO_kwh").innerHTML = Math.round(mg_kwh(no_mgm3, o2, FUELS[fuel].F) * 10) / 10;
+        document.getElementById("output_NO_kwh").innerHTML = Math.round(mg_kwh(no_mgm3, o2, fuel.F) * 10) / 10;
 
     } else {
         document.getElementById("output_NO_kwh").innerHTML = "--";
@@ -148,7 +176,7 @@ function calculate(e) {
         document.getElementById("output_NOx_m3o2").innerHTML = "--";
     }
     if (o2 != "") {
-        document.getElementById("output_NOx_kwh").innerHTML = Math.round(mg_kwh(no_mgm3, o2, FUELS[fuel].F) * 10) / 10;
+        document.getElementById("output_NOx_kwh").innerHTML = Math.round(mg_kwh(no_mgm3, o2, fuel.F) * 10) / 10;
     } else {
         document.getElementById("output_NOx_kwh").innerHTML = "--";
     }
@@ -174,3 +202,13 @@ function mg_kwh(mg_m3, o2, F) {
     return ret;
 }
 
+function co2(fuel, o2) {
+    let co2 = fuel.CO2max * (1 - o2 / O2_MAX);
+    return co2;
+}
+
+function losses(t_gas, t_air, o2, fuel) {
+    let A2 = O2_MAX * fuel.A1 / fuel.CO2max;
+    let loss = (t_gas - t_air) * (fuel.B + A2 / (O2_MAX - o2));
+    return loss;
+}
